7b6eb8c5d238094856f156330a858792c3820af3
Krystian Piękoś <krystian.piekos@infotraining.pl>
2023-10-18 14:44:36 +0200

    Katas

---
+++ mars-rover-kata/src/source.hpp
@@ -0,0 +1,10 @@
+#ifndef SOURCE_HPP
+#define SOURCE_HPP
+
+#include <vector>
+#include <string>
+#include <algorithm>
+
+int foo();
+
+#endif
---
+++ mars-rover-kata/tests/source_tests.cpp
@@ -0,0 +1,13 @@
+#include <algorithm>
+#include <string>
+#include <memory>
+
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+using namespace std;
+
+TEST(Example, ExampleTest)
+{
+    EXPECT_EQ(1, 1);
+}
